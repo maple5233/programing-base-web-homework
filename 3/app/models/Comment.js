5d68d6a530ab16de0127bb6e240b7b48d26a139e
maple5233 <326153996@qq.com>
2016-10-01 03:24:53 +0800

    add Editing Comment functions

--- 3/app/models/Comment.js
+++ 3/app/models/Comment.js
@@ -8,13 +8,19 @@ var CommentSchema = new mongoose.Schema ({
     userId: String,
     comment: String,
     date: String,
-    headImgSrc: String
+    headImgSrc: String,
+    updateDate: String
 }, {strict: true});
 
 CommentSchema.statics = {
     fetch: function (cb) {
         return this
             .find ({})
+            .exec (cb);
+    },
+    fetchById: function (id, cb) {
+        return this
+            .findById (id)
             .exec (cb);
     }
 };
@@ -34,9 +40,68 @@ Comment.$routers = [
                     });
                 }
                 else {
+                    // comments.forEach(msg => {
+                    //     msg.comment = msg.comment.replace('<br />','\n')
+                    // });
                     res.status (200).json ({
                         code: '0',
                         msgs: comments.reverse () // 反转数组 让新的在最前面
+                    });
+                }
+            })
+        }
+    },
+    { // 更新留言
+        method: 'put',
+        path: '/comment',
+        router: (req, res) => {
+            var commentInfo = req.body.commentNew;
+            var id = commentInfo._id;
+            var date = new Date ();
+            var month = date.getMonth () + 1;
+            var dateString;
+            if (month >= 10) {
+                dateString = date.getFullYear () + '年' + month + '月' + date.getDate () + '日 ';
+            } else {
+                dateString = date.getFullYear () + '年0' + month + '月' + date.getDate () + '日 ';
+            }
+            if (date.getHours () >= 10) {
+                dateString += date.getHours ();
+            } else {
+                dateString += '0' + date.getHours ();
+            }
+            if (date.getMinutes () >= 10) {
+                dateString += ':' + date.getMinutes ();
+            } else {
+                dateString += ':0' + date.getMinutes ();
+            }
+            if (date.getSeconds () >= 10) {
+                dateString += ':' + date.getSeconds ();
+            } else {
+                dateString += ':0' + date.getSeconds ();
+            }
+            Comment.fetchById (id, function (err, oldComment) {
+                if (err) {
+                    console.log (err);
+                    res.status (200).json ({
+                        code: '-1'
+                    });
+                } else {
+                    // console.log(oldComment);
+                    oldComment.comment = commentInfo.comment;
+                    oldComment.updateDate = dateString;
+                    oldComment.save (err=> {
+                        if (err) {
+                            console.log (err);
+                            res.status (200).json ({
+                                code: '-2'
+                            });
+                        } else {
+                            res.status (200).json ({
+                                code: '0',
+                                updateDate: dateString
+                            });
+                        }
                     });
                 }
             })
@@ -46,27 +111,26 @@ Comment.$routers = [
         method: 'post',
         path: '/comment',
         router: (req, res) => {
-            var commentInfo = req.body.comment;
+            var commentInfo = req.body.commentNew;
             var date = new Date ();
             var month = date.getMonth () + 1;
             var dateString;
             if (month >= 10) {
                 dateString = date.getFullYear () + '年' + month + '月' + date.getDate () + '日 ';
-            }
-            else {
+            } else {
                 dateString = date.getFullYear () + '年0' + month + '月' + date.getDate () + '日 ';
             }
-            if (date.getHours () > 10) {
+            if (date.getHours () >= 10) {
                 dateString += date.getHours ();
             } else {
                 dateString += '0' + date.getHours ();
             }
-            if (date.getMinutes () > 10) {
+            if (date.getMinutes () >= 10) {
                 dateString += ':' + date.getMinutes ();
             } else {
                 dateString += ':0' + date.getMinutes ();
             }
-            if (date.getSeconds () > 10) {
+            if (date.getSeconds () >= 10) {
                 dateString += ':' + date.getSeconds ();
             } else {
                 dateString += ':0' + date.getSeconds ();
@@ -76,7 +140,8 @@ Comment.$routers = [
                 userId: commentInfo.userId,
                 comment: commentInfo.comment,
                 headImgSrc: randHeadImgSrc,
-                date: dateString
+                date: dateString,
+                updateDate: dateString
             });
             newComment.save (function (err) {
                 if (err) console.log (err);
--- 3/app/public/javascripts/index.js
+++ 3/app/public/javascripts/index.js
@@ -6,14 +6,15 @@ var vm = new Vue ({
         textNumLimit: 140,
         userId: '',
         comment: '',
-        msgs: []
+        msgs: [],
+        editing: false
     },
     ready() {
         /* get msgs in LocalStorage*/
         if (localStorage.getItem ('messages')) {
-            console.log('Load localStorage successfully!');
-            var msgLoad =localStorage.getItem ('messages');
-            var msgObj = JSON.parse(msgLoad);
+            console.log ('Load localStorage successfully!');
+            var msgLoad = localStorage.getItem ('messages');
+            var msgObj = JSON.parse (msgLoad);
             // console.dir(msgObj);
             this.msgs = msgObj;
         }
@@ -22,10 +23,14 @@ var vm = new Vue ({
     },
     methods: {
         sendMsg: function () {
+            if (this.userId.length*this.comment.length === 0) {
+                window.alert('Lost Info!');
+                return;
+            }
             this.$http.post ('/comment', {
-                comment: {
+                commentNew: {
                     userId: this.userId,
-                    comment: this.comment
+                    comment: this.comment.replace(/\n/g,'<br />')
                 }
             }).then (result => {
                 if (result.data.code === '0') {
@@ -33,14 +38,13 @@ var vm = new Vue ({
                     this.comment = '';
                     this.textNumLimit = 140;
                     window.alert ('发送成功!');
-                    console.log ('send successfully!');
+                    // console.log ('send successfully!');
                 } else {
                     console.log (result.data.code);
                 }
             });
         },
         pullMsg: function () {
-            // console.log('pulling msgs...');
             var pullMsgId = setInterval (()=> {
                 this.$http.get ('/comment').then (result => {
                     if (result.data.code === '0') {
@@ -49,33 +53,81 @@ var vm = new Vue ({
                         var newLength = newMsgs.length;
                         var addLength = newLength - this.msgs.length;
                         for (var i = addLength - 1; i >= 0; i--) {
-                            // console.log (newMsgs[ i ]);
+                            newMsgs[ i ].textNumLimit = 140 - newMsgs[ i ].comment.length;
+                            newMsgs[ i ].editing = false; // 新增加两个只存在于前端的属性
                             this.msgs.unshift (newMsgs[ i ]);
                         }
                         /* update LocalStorage*/
-                        var len = this.msgs.length > 5 ? 5 : this.msgs.length;
-                        var latestMsgs = this.msgs.slice(-5); // 深拷贝最旧的5条;
-                        // latestMsgs=[];
-                        // console.log(latestMsgs);
-                        localStorage.setItem ('messages', JSON.stringify(latestMsgs));// 更新
+                        var len = this.msgs.length > 20 ? 20 : this.msgs.length;
+                        var latestMsgs = this.msgs.slice (-len); // 深拷贝最旧的20条;
+                        latestMsgs=[];
+                        localStorage.setItem ('messages', JSON.stringify (latestMsgs));// 更新
                     } else {
                         console.log (result.data.code);
                     }
                 });
-            }, 3000);
+            }, 1000);
         },
         inputComment: function () {
             var len = this.comment.length;
             this.textNumLimit = 140 - len;
+        },
+        inputUpDateComment: function (index) {
+            var len = this.msgs[ index ].comment.length;
+            this.msgs[ index ].textNumLimit = 140 - len;
         },
         checkComment: function () {
             if (event.ctrlKey && event.keyCode == 86) { //禁止粘贴
                 event.returnValue = false;
             }
             var len = this.comment.length;
-            if (len >= 140 && event.keyCode !== 8 && (!event.ctrlKey || !event.keyCode == 65)) {
+            if (len >= 140 && event.keyCode !== 8 && (!event.ctrlKey || !event.keyCode == 65)) { //仅允许删除和全选
                 event.returnValue = false;
             }
+        },
+        checkUpDateComment: function (index) {
+            if (event.ctrlKey && event.keyCode == 86) { //禁止粘贴
+                event.returnValue = false;
+            }
+            var len = this.msgs[ index ].comment.length;
+            if (len >= 140 && event.keyCode !== 8 && (!event.ctrlKey || !event.keyCode == 65)) { //仅允许删除和全选
+                event.returnValue = false;
+            }
+        },
+        editComment: function (index) {
+            if (this.editing === true) { //正在编辑其他的
+                window.alert ('您有其他编辑未保存!');
+                return;
+            } else {
+                this.msgs[ index ].editing = true;
+                this.editing = true;
+                this.msgs[ index ].comment = this.msgs[ index ].comment.replace(/<br\s*\/?>/g,'\n');
+            }
+        },
+        updateMsg: function (index) {
+            this.editing = false;
+            this.msgs[ index ].editing = false;
+            this.msgs[ index ].comment = this.msgs[ index ].comment.replace(/\n/g,'<br />');
+            /* 立即上传 */
+            this.$http.put ('/comment', {
+                commentNew: {
+                    _id: this.msgs[ index ]._id,
+                    comment: this.msgs[ index ].comment.replace(/\n/g,'<br />')
+                }
+            }).then (result => {
+                if (result.data.code === '0') {
+                    this.msgs[ index ].updateDate = result.data.updateDate;
+                    window.alert ('更新成功!');
+                    // console.log ('update successfully!');
+                    /* update LocalStorage*/
+                    var len = this.msgs.length > 20 ? 20 : this.msgs.length;
+                    var latestMsgs = this.msgs.slice (-len); // 深拷贝最旧的20条;
+                    // latestMsgs=[];
+                    localStorage.setItem ('messages', JSON.stringify (latestMsgs));// 更新
+                } else {
+                    console.log (result.data.code);
+                }
+            });
         }
     }
 })
