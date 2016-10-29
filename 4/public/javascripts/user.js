d811b2af50f468a388a949794962d5766423f1d0
maple5233 <326153996@qq.com>
2016-10-30 00:00:10 +0800

    fix(user): 提供表单非空验证

--- 4/public/javascripts/user.js
+++ 4/public/javascripts/user.js
@@ -4,7 +4,7 @@ var vm = new Vue({
       active: {
         now: 4
       },
-      rank: 0, // 用户权限
+      rank: 5, // 用户权限
       classId : '1',
       authorId : '2014150120',
       postTexts: [],
@@ -43,15 +43,15 @@ var vm = new Vue({
       }
       let mouth = (date.getMonth() + 1).toString()
       if (mouth.length === 1) {
-        mouth = '0'+ day
+        mouth = '0'+ mouth
       }
       let hour = date.getHours().toString()
       if (hour.length === 1) {
-        hour = '0'+ day
+        hour = '0'+ hour
       }
       let minute = date.getMinutes().toString()
       if (minute.length === 1) {
-        minute = '0'+ day
+        minute = '0'+ minute
       }
       return `${year}-${mouth}-${day} ${hour}:${minute}`
     },
@@ -64,15 +64,15 @@ var vm = new Vue({
       }
       let mouth = (date.getMonth() + 1).toString()
       if (mouth.length === 1) {
-        mouth = '0'+ day
+        mouth = '0'+ mouth
       }
       let hour = date.getHours().toString()
       if (hour.length === 1) {
-        hour = '0'+ day
+        hour = '0'+ hour
       }
       let minute = date.getMinutes().toString()
       if (minute.length === 1) {
-        minute = '0'+ day
+        minute = '0'+ minute
       }
       return `${year}-${mouth}-${day}`
     },
@@ -89,25 +89,7 @@ var vm = new Vue({
     changeTab: function (index, active) {
       active.now = index
       this.postTexts = []
-      this.newPost = {
-        classId: null,
-        className: null,
-        stuId: null,
-        authorId: null,
-        date: null,
-        deadline: null,
-        gotten: null,
-        num: null,
-        bond: null,
-        howmuch: null,
-        payedMembers: null,
-        unpayedMembers: null,
-        place: null,
-        pAgree: null,
-        pDisagree: null,
-        title: null,
-        contant: null
-      }
+      this.emptyPost()
       this.newPost.authorId = this.authorId
       this.newPost.classId = this.classId
       /** ajax更新视图 */
@@ -134,33 +116,7 @@ var vm = new Vue({
       }
       this.postTexts.push(newPost)
     },
-    submitPost: function (index) {
-      try {
-        /* 字符串处理为数组 */
-        this.newPost.payedMembers = JSON.parse('[' + this.newPost.payedMembers + ']')
-        this.newPost.unpayedMembers = JSON.parse('[' + this.newPost.unpayedMembers + ']')
-        this.newPost.pAgree = JSON.parse('[' + this.newPost.pAgree + ']')
-        this.newPost.pDisagree = JSON.parse('[' + this.newPost.pDisagree + ']')
-        /* 时间转化为时间对象 */
-        this.newPost.date = new Date(this.newPost.date)
-        this.newPost.deadline = new Date(this.newPost.deadline)
-        /* 补全信息 */
-        this.newPost.authorId = this.authorId
-        this.newPost.classId = this.classId
-      } catch(e) {
-        widows.alert('数据格式不正确!')
-        console.log(e)
-        return
-      }
-      console.log(this.newPost)
-      switch (index) {
-        case 0:
-          // ajax
-          break;
-        default:
-          break;
-      }
-      this.postTexts.push(this.newPost)
+    emptyPost : function () {
       // 重置表单
       this.newPost = {
         classId: null,
@@ -181,6 +137,77 @@ var vm = new Vue({
         title: null,
         contant: null
       }
+    },
+    submitPost: function (index) {
+      let datePattern = /^(?:(?!0000)[0-9]{4}-(?:(?:0[1-9]|1[0-2])-(?:0[1-9]|1[0-9]|2[0-8])|(?:0[13-9]|1[0-2])-(?:29|30)|(?:0[13578]|1[02])-31)|(?:[0-9]{2}(?:0[48]|[2468][048]|[13579][26])|(?:0[48]|[2468][048]|[13579][26])00)-02-29)$/
+      try {
+        /* 字符串处理为数组 */
+        this.newPost.payedMembers = JSON.parse('[' + this.newPost.payedMembers + ']')
+        this.newPost.unpayedMembers = JSON.parse('[' + this.newPost.unpayedMembers + ']')
+        this.newPost.pAgree = JSON.parse('[' + this.newPost.pAgree + ']')
+        this.newPost.pDisagree = JSON.parse('[' + this.newPost.pDisagree + ']')
+        /* 时间转化为时间对象再转字符串 */
+        if (this.newPost.date !== null && !this.newPost.date.match(datePattern)) {
+          window.alert('时间格式: 年-月-日')
+          return
+        }
+        if (this.newPost.deadline !== null && !this.newPost.deadline.match(datePattern)) {
+          window.alert('时间格式: 年-月-日')
+          return
+        }
+        this.newPost.date = new Date(this.newPost.date).toString()
+        this.newPost.deadline = new Date(this.newPost.deadline).toString()
+        /* 补全信息 */
+        this.newPost.authorId = this.authorId
+        this.newPost.classId = this.classId
+      } catch(e) {
+        widows.alert('数据格式不正确!')
+        console.log(e)
+        return
+      }
+      let post = this.newPost
+      switch (index) {
+        case 0:
+          if (post.stuId == null || post.className == null || post.date == null) {
+            window.alert('信息不全')
+            this.emptyPost()
+            return
+          }
+          break;
+        case 1:
+          if (post.stuId == null || post.title == null || post.bond == null) {
+            window.alert('信息不全')
+            this.emptyPost()
+            return
+          }
+          break;
+        case 2:
+          if (post.howmuch == null || post.payedMembers == null || post.unpayedMembers == null) {
+            window.alert('信息不全')
+            this.emptyPost()
+            return
+          }
+          break;
+        case 3:
+          if (post.date == null || post.place == null || post.num == null || post.gotten == null 
+            || post.title == null || post.pAgree == null || post.pDisagree == null) {
+            window.alert('信息不全')
+            this.emptyPost()
+            return
+          }
+          break;
+        case 4:
+          if (post.title == null || post.contant == null || post.date == null) {
+            window.alert('信息不全')
+            this.emptyPost()
+            return
+          }
+          break;
+        default:
+          break;
+      }
+      this.postTexts.push(this.newPost)
+      this.emptyPost()
       this.newPost.authorId = this.authorId
       this.newPost.classId = this.classId
     }
