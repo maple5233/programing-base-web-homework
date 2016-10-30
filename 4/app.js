4558a120dbf82131d04a30918a4786d26b038241
maple5233 <326153996@qq.com>
2016-10-30 15:21:16 +0800

    init(backend): 基本配置

--- 4/app.js
+++ 4/app.js
@@ -1,40 +1,49 @@
-let express = require('express');
-let path = require('path');
-let favicon = require('serve-favicon');
-let logger = require('morgan');
-let cookieParser = require('cookie-parser');
-let bodyParser = require('body-parser');
+let express = require ('express');
+let path = require ('path');
+let favicon = require ('serve-favicon');
+let logger = require ('morgan');
+let cookieParser = require ('cookie-parser');
+let bodyParser = require ('body-parser');
+let routes = require ('./routes/index');
+var session = require ('express-session');
+var flash = require ('express-flash');
+let app = express ();
 
-let routes = require('./routes/index');
+// 配置mongoose
+// var mongoose = require ('./mongoose.js');
+// mongoose.db.on ('error', console.error.bind (console, '连接错误!'));
+// mongoose.db.once ('open', function () {
+//     console.log ('连接数据库成功!')
+// });
+// var Comment = require ('./models/Comment');
 
-let app = express();
-
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', routes);
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
-});
-
-// error handlers
-
-// production error handler
-// no stacktraces leaked to user
-app.use(function(err, req, res, next) {
-  res.status(err.status || 500);
+// 配置解析url json
+app.use (bodyParser.json ());
+app.use (bodyParser.urlencoded ({extended: false}));
+app.use (cookieParser ());
+// 配置session
+app.use (session ({
+    secret: 'hjy-2014150120',
+    cookie: {
+        maxAge: 6000000
+    },
+    resave: true,
+    saveUninitialized: true
+}));
+// 开启express消息提示
+app.use (flash ());
+// 配置icon
+app.use (favicon (__dirname + '/public/images/vue_64px.png'));
+// 配置静态文件
+app.use (logger ('dev'));
+app.use (express.static (path.join (__dirname, 'public')));
+// 基本路由
+app.use ('/', routes);
+// 404路由
+app.use (function (req, res, next) {
+    var err = new Error ('Not Found');
+    err.status = 404;
+    req.sendfile (path.resolve (__dirname, 'views/404.html'));
 });
 
 module.exports = app;
--- 4/routes/index.js
+++ 4/routes/index.js
@@ -1,10 +1,14 @@
 "use strict";
 var express = require('express');
+var path = require ("path");
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-   
+    // if (req.session.Id) {
+    //     res.redirect ('/menu/' + req.session.Id);
+    // }
+    res.sendFile (path.resolve (__dirname, '../views/index.html'));
 });
 
 module.exports = router;
