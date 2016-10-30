98ce96ecf0afe6e0374f72da7b44576d697dec4a
maple5233 <326153996@qq.com>
2016-10-30 15:38:27 +0800

    fix(app): 增加404路由

--- 4/Models/user.js
+++ 4/Models/user.js
@@ -2,3 +2,50 @@
  * 用户表user
  * Created by hongjiyao_2014150120 on 16-10-30.
  */
+
+'use strict';
+var mongoose = require ('../mongoose');
+
+var UserSchema = new mongoose.Schema ({
+
+}, {strict: true}); // 开启严格模式，不允许不同数据结构存入
+
+UserSchema.statics = { //静态方法
+    fetch: function (cb) { // 取出所有数据
+        return this
+            .find ({})
+            .exec (cb);
+    },
+    fetchById: function (id, cb) { //根据_id找数据
+        return this
+            .findById (id)
+            .exec (cb);
+    }
+};
+
+var User = mongoose.model ('User', UserSchema);
+
+User.$routers = [
+    { // 获取所有留言
+        method: 'get',
+        path: '/',
+        router: (req, res) => {
+            User.fetch ((err, users)=> {
+                if (err) {
+                    console.log (err);
+                    res.status (200).json ({
+                        code: '-1'
+                    });
+                }
+                else {
+                    res.status (200).json ({
+                        code: '0',
+                        msgs: users.reverse () // 反转数组 让新的在最前面
+                    });
+                }
+            })
+        }
+    }
+];
+
+module.exports = User;
--- 4/app.js
+++ 4/app.js
@@ -7,16 +7,16 @@ let bodyParser = require ('body-parser');
 let routes = require ('./routes/index');
 let session = require ('express-session');
 let flash = require ('express-flash');
-let debug = require('debug')('4:server');
+let debug = require ('debug') ('4:server');
 let app = express ();
 
 // 配置mongoose
 var mongoose = require ('./mongoose.js');
 mongoose.db.on ('error', console.error.bind (console, '连接错误!'));
 mongoose.db.once ('open', function () {
-    debug('连接数据库成功!')
+    debug ('连接数据库成功!')
 });
-// var Comment = require ('./models/Comment');
+// var User = require ('./models/User');
 
 // 配置解析url json
 app.use (bodyParser.json ());
@@ -41,10 +41,8 @@ app.use (express.static (path.join (__dirname, 'public')));
 // 基本路由
 app.use ('/', routes);
 // 404路由
-app.use (function (req, res, next) {
-    var err = new Error ('Not Found');
-    err.status = 404;
-    req.sendfile (path.resolve (__dirname, 'views/404.html'));
+app.use (function (req, res) {
+    res.sendFile (path.resolve (__dirname, 'views/404.html'));
 });
 
 module.exports = app;
