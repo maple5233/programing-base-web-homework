13445804c75e653e1fbfcdba154ce79146a04528
maple5233 <326153996@qq.com>
2016-10-31 16:16:45 +0800

    init(model): 增加收班费帖子的model定义

---
+++ 4/Models/classmoney.js
@@ -0,0 +1,47 @@
+/**
+ * 收班费帖
+ * Created by hongjiyao_2014150120 on 16-10-31.
+ */
+'use strict';
+let mongoose = require ('../mongoose');
+let classMoneySchema = new mongoose.Schema ({
+    classId: {
+        type: String,
+        required: true,
+        index: true
+    }, // 班级ID
+    date: {
+        type: Date,
+        required: true
+    }, // 截止时间
+    payedMembers: {
+        type: [Number],
+        required: true
+    }, // 已经缴费的人的ID
+    unpayedMembers: {
+        type: [Number],
+        required: true
+    }, // 尚未缴费的人的ID
+    authorId: {
+        type: Number,
+        required: true
+    } // 发帖人ID
+}, {strict: true});
+
+classMoneySchema.statics = { //静态方法
+    fetch: function (cb) { // 取出所有数据
+        return this
+            .find ({})
+            .exec (cb);
+    },
+    fetchById: function (id, cb) { //根据_id找数据
+        return this
+            .findById (id)
+            .exec (cb);
+    }
+};
+
+
+
+let classMoney = mongoose.model ('ClassMoney', classMoneySchema);
+module.exports = classMoney;
