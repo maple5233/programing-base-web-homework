aee41f399c4e25cee0456322ebc319faad8f4ef9
maple5233 <326153996@qq.com>
2016-11-04 17:09:49 +0800

    feat(client-index): 增加一个注册表单项目：班级

--- 4/public/javascripts/index.js
+++ 4/public/javascripts/index.js
@@ -4,6 +4,7 @@ import { hex_sha1 } from "./libs/sha1";
 var vm = new Vue ({
     el: '#whole',
     data: {
+        userClass : '',
         userName : '',
         userPass : '',
         _userPass : '' // 加密后的密码;
@@ -12,6 +13,13 @@ var vm = new Vue ({
         verify : function () {
             if (this.userName.length === 0 || this.userPass.length === 0) {
                 window.alert("信息不完整!")
+                return false
+            }
+            if (this.userName.length > 10) {
+                window.alert ('登录名过长！')
+                return false
+            } else if (this.userPass.length < 6) {
+                window.alert ('密码过短！')
                 return false
             }
             this._userPass = hex_sha1(this.userPass)
@@ -23,9 +31,20 @@ var vm = new Vue ({
             }
         },
         register : function () {
-            if (this.verify()) {
-                console.log('用户请求注册')
+            if (!this.verify()) {
+                return
             }
+            if (this.userClass.length === 0) {
+                window.alert('信息不完整!')
+                return
+            }
+            this.$http.post ('/register', {
+                registerName: this.userName,
+                registerPass: this._userPass,
+                registerClass: this.userClass
+            }).then (function (result) {
+                let res = result.data
+            })
         }
     }
 })
