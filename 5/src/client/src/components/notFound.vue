786a6218dc6008973a6ea87c7d005459a98929ca
maple5233 <326153996@qq.com>
2016-12-30 01:19:27 +0800

    feat(404): 404页面

---
+++ 5/src/client/src/components/notFound.vue
@@ -0,0 +1,18 @@
+<template>
+  <main class="main not-found">
+    <h1>404</h1>
+    <h2>Page not found</h2>
+  </main>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style>
+  .not-found {
+    text-align: center;
+    padding: 30px;
+  }
+</style>
