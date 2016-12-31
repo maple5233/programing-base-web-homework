a6c3ef673ecb98fd3f55e661296b26199509f6c6
maple5233 <326153996@qq.com>
2016-12-31 20:53:24 +0800

    feat(util): 写json结果的工具函数

---
+++ 5/src/routes/utils/writeJson.js
@@ -0,0 +1,25 @@
+/**
+ * 写返回结果公共函数
+ * Created by hongjiyao_2014150120 on 16-12-31.
+ */
+
+/**
+ * 写返回结果
+ * @param res 请求
+ * @param ret 返回结果 出错为null
+ * @param success 请求是否成功
+ * @param statusNum 状态码
+ * @param msg 报错信息
+ */
+let jsonWrite = function(res, ret, success, statusNum, msg) {
+    if(!success) {
+        res.status(statusNum).json({
+            code: -1,
+            msg: msg
+        });
+    } else {
+        res.status(200).json(ret);
+    }
+};
+
+module.exports = jsonWrite;
