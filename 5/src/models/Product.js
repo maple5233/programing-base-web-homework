413e13fd135d438de893ecad1fb590487088e712
maple5233 <326153996@qq.com>
2016-12-31 20:07:03 +0800

    feat(productModel): 产品表模型

---
+++ 5/src/models/Product.js
@@ -0,0 +1,43 @@
+/**
+ * 产品模型
+ * Created by hongjiyao_2014150120 on 16-12-31.
+ */
+let mongoose = require('mongoose');
+let Schema = mongoose.Schema;
+
+let ProductSchema = new Schema({
+    productName: {
+        type: String,
+        required: true
+    },  // 名称
+    productDetails: {
+        type: String,
+        required: true
+    },  // 描述
+    productPrice: {
+        type: Number,
+        required: true
+    },  // 价格
+    productInventory: {
+        type: Number,
+        min: 0,
+        default: 0
+    }   // 库存
+});
+
+ProductSchema.statics = {
+    fetchById: function(id, cb) {
+        return this.findById(id)
+                   .exec(cb);
+    },
+    fetch: function(json, cb) {
+        return Product.find(json)
+                      .sort({
+                          '_id': -1
+                      })
+                      .exec(cb);
+    }
+};
+
+let Product = mongoose.model('Product', ProductSchema);
+module.exports = Product;
