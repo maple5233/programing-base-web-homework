20017a9e2812eadcbac0d0a17f89c328b09aaf0f
maple5233 <326153996@qq.com>
2016-12-31 21:45:40 +0800

    refrator(something): 排版

--- 5/src/app.js
+++ 5/src/app.js
@@ -6,15 +6,23 @@ const favicon = require('serve-favicon');
 const logger = require('morgan');
 const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
-require ('babel-core/register');
+require('babel-core/register');
 const app = express();
 // 引入jwt模块
-const jwt = require ('jwt-simple');
+const jwt = require('jwt-simple');
 // 数据库配置文件
 const mongoose = require('mongoose');
-const debug = require ('debug') ('4:server');
-const config = require ('./config');
-
+const debug = require('debug')('4:server');
+const config = require('./config');
+// 连接数据库
+config.db.on('error', () => {
+    debug('连接数据库未成功!');
+    console.error.bind(console, '连接错误!');
+});
+config.db.once('open', ()=> {
+    console.log('连接数据库成功!');
+    debug('连接数据库成功!')
+});
 
 /**
  * 引入各种REST路由文件
@@ -29,18 +37,18 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, '/client')));
 // 配置icon
-app.use (favicon (__dirname + '/client/src/assets/vue_64px.png'));
+app.use(favicon(__dirname + '/client/src/assets/vue_64px.png'));
 
 // 基本路由
 app.get('/', function(req, res, next) {
-    res.sendFile(path.resolve (__dirname, './client/index.html'));
+    res.sendFile(path.resolve(__dirname, './client/index.html'));
 });
 
 /**
  * safe路由不用经过jwt
  */
-safeAPI.$routers.forEach (router => {
-    app[ router.method ] ('/api' + router.path, router.router);
+safeAPI.$routers.forEach(router => {
+    app[ router.method ]('/api' + router.path, router.router);
 });
 
 /**
@@ -55,41 +63,41 @@ safeAPI.$routers.forEach (router => {
  * restful路由
  */
 //  restful路由
-[ userAPI ].forEach (item => {
-    item.$routers.forEach (router => {
-        app[ router.method ] ('/api' + router.path, router.router);
+[ userAPI ].forEach(item => {
+    item.$routers.forEach(router => {
+        app[ router.method ]('/api' + router.path, router.router);
     })
 });
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  let err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+    let err = new Error('Not Found');
+    err.status = 404;
+    next(err);
 });
 
 // error handlers
 
 // development error handler
 // will print stacktrace
-if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.end({
-      message: err.message,
-      error: err
+if(app.get('env') === 'development') {
+    app.use(function(err, req, res, next) {
+        res.status(err.status || 500);
+        res.end({
+            message: err.message,
+            error: err
+        });
     });
-  });
 }
 
 // production error handler
 // no stacktraces leaked to user
 app.use(function(err, req, res, next) {
-  res.status(err.status || 500);
-  res.end('error', {
-    message: err.message,
-    error: {}
-  });
+    res.status(err.status || 500);
+    res.end('error', {
+        message: err.message,
+        error: {}
+    });
 });
 
 
--- 5/src/config.js
+++ 5/src/config.js
@@ -3,9 +3,7 @@
  * Created by hongjiyao_2014150120 on 16-12-31.
  */
 const mongoose = require ('mongoose');
-const debug = require ('debug') ('4:server');
 mongoose.Promise = global.Promise;
 module.exports.db = mongoose.connection;
 module.exports = mongoose.createConnection ('mongodb://localhost/MyTaoBao');
 module.exports.Schema = mongoose.Schema;
-module.exports.debug = debug;
