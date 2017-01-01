1fd2de53f563decaf27177910c0805bdbb0a601f
maple5233 <326153996@qq.com>
2017-01-01 21:53:31 +0800

    feat(404): 把404页面萌化

--- 5/src/client/src/components/notFound.vue
+++ 5/src/client/src/components/notFound.vue
@@ -1,7 +1,9 @@
 <template>
   <main class="main not-found">
     <h1>404</h1>
-    <h2>Page not found</h2>
+    <h2>阿叻，你好像来到了异次元</h2>
+    <br/>
+    <h2>点<router-link to="/">这里</router-link>回到起点</h2>
   </main>
 </template>
 
